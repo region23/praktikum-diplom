f44f0804a1dc6254a50e23a665a28a647e2600da
Pavel Pavlenko <region23@gmail.com>
2022-07-25 09:58:24 +0300

    fix

--- internal/external_api/accrual.go
+++ internal/external_api/accrual.go
@@ -15,9 +15,9 @@ import (
 )
 
 type AccuralType struct {
-	Order   string  `json:"order"`             // номер заказа
-	Status  string  `json:"status"`            // статус расчёта начисления
-	Accrual float64 `json:"accrual,omitempty"` // рассчитанные баллы к начислению, при отсутствии начисления — поле отсутствует в ответе
+	Order   string              `json:"order"`             // номер заказа
+	Status  storage.OrderStatus `json:"status"`            // статус расчёта начисления
+	Accrual float64             `json:"accrual,omitempty"` // рассчитанные баллы к начислению, при отсутствии начисления — поле отсутствует в ответе
 }
 
 // получение информации о расчёте начислений баллов лояльности
